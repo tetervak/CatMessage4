fc1aa51fdc069e82ec4f24243ad73b0a8830c7fb
Alex Tetervak <tetervak@sheridancollege.ca>
2019-02-12 12:29:38 -0500

    initial commit

---
+++ app/src/main/java/ca/javateacher/catmessage4/MainActivity.java
@@ -0,0 +1,47 @@
+package ca.javateacher.catmessage4;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity
+    implements InputFragment.InputListener{
+
+  private static final String ABOUT_FRAGMENT = "aboutFragment";
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+    Toolbar toolbar = findViewById(R.id.toolbar);
+    setSupportActionBar(toolbar);
+  }
+
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    // Inflate the menu; this adds items to the action bar if it is present.
+    getMenuInflater().inflate(R.menu.menu_main, menu);
+    return true;
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    if (item.getItemId() == R.id.about) {
+      AboutFragment aboutFragment = AboutFragment.newInstance();
+      aboutFragment.show(getSupportFragmentManager(), ABOUT_FRAGMENT);
+      return true;
+    }
+    return super.onOptionsItemSelected(item);
+  }
+
+  @Override
+  public void updateMessage(String text) {
+    OutputFragment outputFragment =
+        (OutputFragment) getSupportFragmentManager()
+            .findFragmentById(R.id.fragment_output);
+    if(outputFragment != null)
+      outputFragment.updateMessage(text);
+  }
+}
---
+++ app/src/main/java/ca/javateacher/catmessage4/OutputFragment.java
@@ -0,0 +1,54 @@
+package ca.javateacher.catmessage4;
+
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class OutputFragment extends Fragment {
+
+
+  private static final String MESSAGE = "message";
+  private String mTextMessage;
+  private TextView mMessageView;
+
+  public OutputFragment() {
+    // Required empty public constructor
+  }
+
+  @Override
+  public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                           Bundle savedInstanceState) {
+    // Inflate the layout for this fragment
+    View view = inflater.inflate(R.layout.fragment_output, container, false);
+    mMessageView = view.findViewById(R.id.message_text);
+    if(savedInstanceState != null){
+      mTextMessage = savedInstanceState.getString(MESSAGE);
+      mMessageView.setText(mTextMessage);
+    }else{
+      mTextMessage = mMessageView.getText().toString();
+    }
+
+    return view;
+  }
+
+  public void updateMessage(String text) {
+    mTextMessage = text;
+    mMessageView.setText(text);
+  }
+
+  @Override
+  public void onSaveInstanceState(@NonNull Bundle outState) {
+    super.onSaveInstanceState(outState);
+    outState.putString(MESSAGE, mTextMessage);
+  }
+
+}
